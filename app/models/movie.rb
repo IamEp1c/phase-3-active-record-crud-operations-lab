7963153e0c16c8ca340f28c4c019f22190274914
Waqas Anjum <Qanjum96@gmail.com>
2022-05-23 19:54:01 -0400

    commit tree 891c1100ebf886ca30aa90e3153311897d4eabe7

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,34 @@
 class Movie < ActiveRecord::Base
 
+    def self.create_with_title(title)
+        # user = User.create(name: "David", occupation: "Code Artist")
+        Movie.create(title: title)
+    end
+
+    def self.first_movie()
+        Movie.all.first
+    end
+
+    def self.last_movie()
+        Movie.all.last
+    end
+
+    def self.movie_count()
+        Movie.all.count
+    end
+
+    def self.find_movie_with_id(id)
+        Movie.all.find(id)
+        puts id
+    end
+
+    def self.find_movie_with_attributes(attributes)
+        Movie.find_by(attributes)
+        puts attributes
+    end
+
+
 end
+
+
+
